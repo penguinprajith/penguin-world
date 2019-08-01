03eb008f13af71455c67a0156ec83227ac2db573
penguinprajith <penguinprajith@gmail.com>
2019-08-01 21:00:48 +0530

    bit operation macros and functions added

---
+++ ds/bitops.cpp
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include "bitops.h"
+
+
+
+int last1bit(int x)
+{
+	return x & -x;	
+}
+
+void printbinary(int x)
+{
+	int mask = 1 << 31;
+	for(int count = 1; count < 33; ++count)
+	{
+		(x & mask) ? putchar('1'):putchar('0');
+		x = x << 1; 	
+		if(count %4 == 0)
+			std::cout << " ";
+	}
+	std::cout << std::endl;	
+	
+}
+
+int bitreverse(int x)
+{
+	x = (x & 0x55555555) << 1 | (x & 0xAAAAAAAA) >> 1;
+	x = (x & 0x33333333) << 2 | (x & 0xCCCCCCCC) >> 2;
+	x = (x & 0x0F0F0F0F) << 4 | (x & 0xF0F0F0F0) >> 4;
+	x = (x & 0x00FF00FF) << 8 | (x & 0xFF00FF00) >> 8;
+	x = (x & 0x0000FFFF) << 16 | (x & 0xFFFF0000) >> 16;
+	
+	return x;
+}
+
+int paritycount(int x)
+{
+	int count = 0;
+	int v = x;
+	while(v)
+	{
+		count++;
+		v = v & v-1;
+	}
+	return count;	
+}
+
+int onecount(int x)
+{
+	return paritycount(x);
+}
+
+int zerocount(int x)
+{
+	return (sizeof(x)*8) - onecount(x);	
+}
+
+int abs(int x)
+{
+	x = (x ^ (x >> 31)) - (x >> 31);	
+	return x;
+}
+
+
---
+++ ds/bitops.h
@@ -0,0 +1,50 @@
+#ifndef BITOPS_H
+#define BITOPS_H
+
+//macros to set,reset and toggle bits
+#define _setbit(x,bitno) 	(x |= (1<< bitno))
+#define _resetbit(x,bitno)	(x &= ~(1<< bitno))
+#define _togglebit(x,bitno)	(x ^= (1<< bitno))
+
+//to determine whether the give number is power of 2
+#define _ispower2(x)		((x & (x-1)) == 0)
+
+//simple xor logic to determine number equivalence
+#define _isequal(x,y)		((x ^ y) == 0)	
+
+
+//set n bits of a variable x starting from index i to a given value b(0/1)
+// 'i' is the index
+// 'n' is number of bits
+// 'b' is bitvalue (0 or 1)
+// 'x' is the variable
+// This can be used when the data in the memory location is not known and we
+// need to set/reset the bits forcefully
+
+#define _mask(i,n)			(((1<<n)-1) << i) 
+#define _resetx(x,i,n)		( (x & _mask(i,n)) ^ x )
+#define _setx(x,i,n)		( (_resetx(x,i,n) ^ _mask(i,n)) )
+#define _if(x,i,n,b)		( (((b << 31)>>31) & _setx(x,i,n)) | \
+							(((!b << 31)>>31) & _resetx(x,i,n)))
+
+
+#define _setvalue(x,i,n,b)	(x = (_if(x,i,n,b)))
+
+#define _extractbits(x,i,n) ( x & _mask(i,n))
+
+#define _swapn(x)	( ((x & 0x0F) << 4) | ((x & 0xF0) >> 4) )
+#define _swapb(x)	( ((x & 0x00FF) << 8) | ((x & 0xFF00) >> 8) )
+#define _swapw(x)	( ((x & 0x000000FF) << 24) | ((x & 0xFF000000) >> 24) | ((x & 0x0000FF00) << 8) | ((x & 0x00FF0000) >> 8) )
+
+
+int last1bit(int x);
+void printbinary(int x);
+int bitreverse(int x);
+int paritycount(int x);
+int onecount(int x);
+int zerocount(int x);
+int abs(int x);
+
+
+#endif
+
